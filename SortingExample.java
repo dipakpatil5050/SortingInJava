9fac5429781355efc966a7baaabe865a502e37ab
Dipak Ukha Patil <92545685+dipakpatil5050@users.noreply.github.com>
2023-04-09 15:35:14 +0530

    Add files via upload

---
+++ SortingExample.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SortingExample {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 9, 7, 2, 1, 4};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr)
+    }
+}
